a828c15dd6d5402826ab3adbba9a9ced6875f408
Ty <tycho.eggen@gmail.com>
2026-03-30 23:03:38 -0700

    docker: docker docker docker docker generate-flavours-matrix docker

--- tools/generate-flavours-matrix-include.sh
+++ tools/generate-flavours-matrix-include.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$(realpath "$0")")/../" || exit 255
 
-echo "flavours=[$(find ./images/ -mindepth 1 -maxdepth 1 -type d | cut -f3 -d/ | xargs -I% echo '"%"' | tr ' ' ',')]"
+echo "flavours=[$(find ./images/ -mindepth 1 -maxdepth 1 -type d | cut -f3 -d/ | xargs -I% echo '"%"' | tr '\n' ',' | perl -pe 's/,$//g')]"
